6ff3225f382fa247bc94f7a1c98ee2744bc71dab
AndyCarmenR <a.carmenr@hotmail.com>
2024-02-22 18:14:34 -0600

    added unit tests

--- akari/src/main/java/com/akari/backend/resources/SceneryResource.java
+++ akari/src/main/java/com/akari/backend/resources/SceneryResource.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class SceneryResource {
 
     @Autowired
-    SceneryService sceneryService;
+    private SceneryService sceneryService;
     @GetMapping(value = "/scenery/{id}")
     public ResponseEntity<CatScenery> getById(@RequestParam long id){
         return new ResponseEntity<>(sceneryService.getSceneryById(id),HttpStatus.OK) ;
